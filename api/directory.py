f77fe88e9dd0cdc8fc510cd9f00ea6a1c30a66a3
Данзан <danzankorneev3@gmail.com>
2023-08-17 15:18:33 +0300

    Danzan new commit

--- api/directory.py
+++ api/directory.py
@@ -1,6 +1,7 @@
 import os
 
 from fastapi import APIRouter
+from starlette.responses import JSONResponse
 
 from models.models import DirectCreate
 
@@ -17,12 +18,13 @@ async def filestor(request: DirectCreate):
         folder_path = os.path.join(r"C:\Users\1\PycharmProjects\FileStorage\filestor", folder_name)
 
     try:
-        os.mkdir(folder_path)
+        os.makedirs(folder_path)
+        #os.mkdir(folder_path)
         print(f"Папка {folder_path} успешно создана.")
-        return "Папка успешно создана"
+        return JSONResponse(content={"message": "Папка успешно создана"}, status_code=200)
     except FileExistsError:
         print(f"Папка {folder_path} уже существует.")
-        return "Папка уже существует"
+        return JSONResponse(content={"message": "Папка уже существует"}, status_code=404)
 
 
 @router.get("/")
@@ -48,8 +50,8 @@ async def dirupdate(old_name: str, new_name: str, paf=None):
             new_path = os.path.join(folder_path, new_name)
             os.rename(old_path, new_path)
             file_system = os.listdir(folder_path)
-            return {"file_system": file_system, "Ответ": "Папка переименнована"}
+            return  JSONResponse(content={"message": "Папка переименнована"}, status_code=200)
         else:
-            return "Такого файла не существует"
+            return JSONResponse(content={"message": "Такого файла не существует"}, status_code=404)
     else:
-        return "Системе не удается найти путь"
+        return JSONResponse(content={"message": "Системе не удается найти путь"}, status_code=404)
--- api/file.py
+++ api/file.py
@@ -2,6 +2,7 @@ import os
 
 from fastapi.responses import FileResponse
 from fastapi import Depends, UploadFile, File, APIRouter
+from starlette.responses import JSONResponse
 
 from models.models import FileCreate, DownloadRequest
 
@@ -19,11 +20,19 @@ async def filecreate(request: FileCreate = Depends(), file: UploadFile = File(No
                 with open(os.path.join(folder_path, file.filename), "wb") as f:
                     f.write(file.file.read())
                     file_system = os.listdir(folder_path)
-                return {"file_system": file_system, "Ответ": "Файл успешно сохранен"}
+                return JSONResponse(content={"message": "Файл успешно сохранен"}, status_code=200)
             file_system = os.listdir(folder_path)
-            return {"file_system": file_system}
+            return JSONResponse(content={"message": "Файл не передан"}, status_code=404)
         else:
-            return "Такого пути не существует"
+            os.makedirs(folder_path)
+            if file:
+                with open(os.path.join(folder_path, file.filename), "wb") as f:
+                    f.write(file.file.read())
+                    file_system = os.listdir(folder_path)
+                return JSONResponse(content={"message": "Файл успешно сохранен"}, status_code=200)
+            file_system = os.listdir(folder_path)
+            return JSONResponse(content={"message": "Файл не передан"}, status_code=404)
+
     else:
         folder_path = r"C:\Users\1\PycharmProjects\FileStorage\filestor"
         if os.path.exists(folder_path):
@@ -31,12 +40,12 @@ async def filecreate(request: FileCreate = Depends(), file: UploadFile = File(No
                 with open(os.path.join(folder_path, file.filename), "wb") as f:
                     f.write(file.file.read())
                 file_system = os.listdir(folder_path)
-                return {"file_system": file_system}
+                return JSONResponse(content={"message": "Файл успешно сохранен"}, status_code=200)
             else:
                 file_system = os.listdir(folder_path)
-                return {"file_system": file_system}
+                return JSONResponse(content={"message": "Файл не передан"}, status_code=404)
         else:
-            return "Такого пути не существует"
+            return JSONResponse(content={"message": "Пути не существует"}, status_code=404)
 
 
 @router.post("/download")
@@ -53,4 +62,22 @@ async def download(request: DownloadRequest):
     if os.path.exists(file_path):
         return FileResponse(file_path, filename=file_name)
     else:
-        return "Файл не найден"
+        return JSONResponse(content={"message": "Такого файла не существует"}, status_code=404)
+
+
+@router.post("/delete")
+async def delete(request: DownloadRequest):
+    file_name = request.file_name
+    paf = request.paf
+    if paf:
+        folder_path = os.path.join(r"C:\Users\1\PycharmProjects\FileStorage\filestor", paf)
+    else:
+        folder_path = os.path.join(r"C:\Users\1\PycharmProjects\FileStorage\filestor")
+
+    file_path = os.path.join(folder_path, file_name)
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        return JSONResponse(content={"message": "Файл удален"}, status_code=200)
+    else:
+        return JSONResponse(content={"message": "Такого файла не существует"}, status_code=404)
